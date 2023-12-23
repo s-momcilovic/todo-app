0ffbcaca5065530f6fc92c7e35b2f5dcb72738dd
s-momcilovic <86606868+s-momcilovic@users.noreply.github.com>
2023-12-23 16:21:42 +0100

    first setup

---
+++ src/components/Todo.js
@@ -0,0 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+export const Todo = () => {
+  return (
+    <div className="Todo">
+      <p>Go to school</p>
+      <div>srbjia</div>
+    </div>
+  );
+};
---
+++ src/components/TodoWrapper.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import { TodoForm } from "./TodoForm";
+import { v4 as uuidv4 } from "uuid";
+// uuidv4();
+
+export const TodoWrapper = () => {
+  const [todos, setTodos] = useState([]);
+  // useEffect(() => {
+  //   console.log(todos);
+  // }, [todos]);
+
+  function addTodo(todo) {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+  }
+  return (
+    <div className="TodoWrapper">
+      <TodoForm addTodo={addTodo} />
+    </div>
+  );
+};
